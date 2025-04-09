d8e31f3919f870276dd11f292452fc5f73c102ec
ethan <ethanvr3@gmail.com>
2025-04-09 14:19:05 +0200

    change path of data

--- src/database/insert.py
+++ src/database/insert.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ElemTree
 import os
 import create 
 
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 
 def insertPlayersData(db, cursor):
     csvPath = os.path.join(ROOT_DIR, 'data', 'joueurs.csv')
