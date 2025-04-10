c161a99af2f2ec3800311a84237bef4d478f77df
ethan <ethanvr3@gmail.com>
2025-04-10 14:03:44 +0200

    add show of characters of the player

--- src/gui/manage_characters.py
+++ src/gui/manage_characters.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QSpinBox, QPushButton, QMessageBox, QApplication, QLabel, QSpacerItem, QSizePolicy, QLineEdit
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QPushButton, QSpacerItem, QSizePolicy, QTableWidgetItem, QMessageBox, QHeaderView
 from PyQt5.QtCore import Qt
 import mysql.connector
 
@@ -20,16 +20,24 @@ class ManageCharacters(QWidget):
             database="rpg"
         )
         self.cursor = self.db.cursor()
-        self.getCharactes()
         self.setup()
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.getCharactes()
+
 
     def setup(self):
         backButton = QPushButton("Back")
         backButton.setFixedWidth(500)
 
+        self.table = QTableWidget()
+        self.table.setFixedSize(500, 500)
+
         mainLayout = QVBoxLayout()
         mainLayout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
         mainLayout.addWidget(qt_config.createCenterBoldTitle("Manage your Characters"), alignment=Qt.AlignCenter)
+        mainLayout.addWidget(self.table, alignment=Qt.AlignCenter)
         mainLayout.addWidget(backButton, alignment=Qt.AlignCenter)
         mainLayout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
@@ -41,8 +49,24 @@ class ManageCharacters(QWidget):
         self.stackedWidget.setCurrentIndex(0) # Go back to the main menu
 
     def getCharactes(self):
-        self.cursor.execute("SELECT p.Name FROM Players p WHERE ID = %s", self.ID)
+        self.cursor.execute("SELECT p.Name FROM Players p WHERE ID = %s", (self.ID,))
         name = self.cursor.fetchone()
 
-        self.cursor.execute("SELECT * FROM Characters WHERE Username = %s", name)
-        self.characters = self.cursor.fetchall()
+        self.cursor.execute("SELECT * FROM Characters WHERE Username = %s", (name))
+        result = self.cursor.fetchall()
+
+        if not result:
+            QMessageBox.information(self, "No character", "You don't have any character yet !")
+            self.table.setRowCount(0)            
+            return
+
+        self.table.setRowCount(len(result))
+        self.table.setColumnCount(8)
+        self.table.setHorizontalHeaderLabels(["Name", "Strenght", "Agility", "Intelligence", "Health", "Mana", "Class", "Username"])
+
+        for rowIdx, rowData in enumerate(result):
+            for colIdx, value in enumerate(rowData):
+                self.table.setItem(rowIdx, colIdx, QTableWidgetItem(str(value)))
+
+        self.table.resizeColumnsToContents()
+        self.table.resizeRowsToContents()
